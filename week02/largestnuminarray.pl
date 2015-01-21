8598a932713e14b6b5adfd3cd32523a1044825d6
David Cheung <davidzors@hotmail.com>
2015-01-21 15:25:57 -0500

    renamed largestnuminarray.pl to moresubs.pl wrote additional code to moresubs.pl

--- week02/largestnuminarray.pl
+++ week02/largestnuminarray.pl
@@ -9,8 +9,9 @@ my @input = qw/1  2 3 4 5 6 7 10 22 15 12 50 23/;
 print (largestnum(@input));
 
 sub largestnum {        #finds largest number in the array
+    my @input = @_;
     my $largest = $_[0];
-    foreach (@_){
+    foreach (@input){
         if ($largest < $_) {
             $largest = $_;
         }
---
+++ week02/moresubs.pl
@@ -0,0 +1,43 @@
+#!usr/bin/perl
+use strict;
+use warnings;
+
+# subroutine that takes an array of numbers and returns the largest value in it
+# it will be able to handle any length of array
+
+my @input = qw/1  2 3 4 5 6 7 10 22 15 12 50 23/;
+my @input2 = (1, "a", 3, 3, "x");
+print (largestnum(@input));
+print "\n";
+print &largestnum(@input);
+print "\n";      #same as "return" in subroutine, returns last executed command
+
+print (find("3", @input2));
+print "\n";
+
+sub largestnum {        #finds largest number in the array
+    my @array = @_;
+    my $largest = $_[0];
+    foreach (@array){
+        if ($largest < $_) {
+            $largest = $_;
+        }
+    }
+    return $largest;
+}
+
+sub find {
+    my $num = $_[0];
+    my @array = @_;
+    shift @array;
+    my $found = 0;
+    my $arraylength = length @array;
+    
+        foreach (@array) {
+            if ($num eq $_) {
+                $found = 1;
+            }
+        }
+    
+    return $found;
+}
